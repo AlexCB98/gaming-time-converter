9d973675c67bb195ffdbf98a52b373735cac94da
Alexandru <lexyus22@gmail.com>
2026-06-07 13:43:07 +0200

    Add converter UI elements

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ FONT = ('Segoe UI', 10)
 
 window = Tk()
 window.title('Gaming Time Converter')
-window.minsize(width= 400, height= 200)
+window.minsize(width= 300, height= 100)
 
-subtitle = Label(text='Hours played. Days revealed.', font= FONT)
-subtitle.grid(column=5, row=0)
+subtitle = Label(text='-> Hours played. Days revealed.', font= FONT)
+subtitle.grid(column=0, row=0, columnspan=3)
+
+text_1 = Label(text= 'If you played', font= FONT)
+text_1.grid(column=0, row=1, padx=5)
 
 choose = Entry(width= 10)
-choose.grid(column=2, row=1)
+choose.grid(column=1, row=1, padx=5)
 
-hours = Label(text= 'Hours', font= FONT)
-hours.grid(column=3, row=1)
+hours_text = Label(text= 'Hours', font= FONT)
+hours_text.grid(column=2, row=1, padx=5)
 
+text_2 = Label(text='That means exactly', font= FONT)
+text_2.grid(column=0 , row=2, padx=5)
+
+result = Label(text='0', font= FONT)
+result.grid(column=1, row=2, padx=5)
+
+days_text = Label(text='Day/s', font= FONT)
+days_text.grid(column=2, row=2, padx=5)
 
 
 
