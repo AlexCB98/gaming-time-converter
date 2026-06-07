40b06f0b5c5d7ab658b0178ecf1a249067c1340a
Alexandru <lexyus22@gmail.com>
2026-06-07 13:19:30 +0200

    Add input field and labels

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from tkinter import *
 
+FONT = ('Segoe UI', 10)
+
 window = Tk()
-window.title('Hours to days converter')
+window.title('Gaming Time Converter')
 window.minsize(width= 400, height= 200)
 
+subtitle = Label(text='Hours played. Days revealed.', font= FONT)
+subtitle.grid(column=5, row=0)
 
+choose = Entry(width= 10)
+choose.grid(column=2, row=1)
 
-
-
-
+hours = Label(text= 'Hours', font= FONT)
+hours.grid(column=3, row=1)
 
 
 
